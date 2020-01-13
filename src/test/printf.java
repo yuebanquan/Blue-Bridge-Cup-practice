71b5c2e3e66d534960a0b041801f04b3d2f9512e
yuebanquan <yuebanquan@gmail.com>
2020-01-13 18:23:28 +0800

    新增题目

---
+++ src/test/printf.java
@@ -0,0 +1,7 @@
+package test;
+
+public class printf {
+    public static void main(String[] args) {
+        System.out.printf("%.1f", 0.94d);
+    }
+}
--- src/入门训练/BEGIN3.java
+++ src/入门训练/BEGIN3.java
@@ -1,5 +1,13 @@
 package 入门训练;
 
-public class BEGIN3 {
+import java.util.Scanner;
 
+public class BEGIN3 {
+    public static void main(String[] args) {
+        final double PI = 3.14159265358979323;
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        double s = Math.pow(r,2) * PI;
+        System.out.printf("%.7f", s);
+    }
 }
